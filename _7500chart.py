d1c9efba4b29ffc70d7b18775cd989cd3110cdc6
林 子千 <lin-zi-qian@outlook.com>
2025-12-10 01:13:52 +0800

    新增右键选择阴性对照并查看平均值，无数据格改为不可选中

--- _7500chart.py
+++ _7500chart.py
@@ -9,6 +9,9 @@ from tkinter import N, filedialog
 import sys
 import os
 
+#button_states = [True] * 96
+text_obj = None
+
 def choose_xls_doc():
     #选择文件
     print("please choose document")
@@ -26,44 +29,6 @@ def choose_xls_doc():
     file_type = os.path.splitext(excel_path)
     return excel_path, file_type 
 
-class ButtonGrid:
-    def __init__(self, rows=8, cols=12):
-        self.rows = rows
-        self.cols = cols
-        self.fig, self.ax = plt.subplots(figsize=(cols*1.2, rows*1.2))
-        self.ax.axis('off')
-        self.buttons = []
-        self.create_grid()
-    
-
-    def create_grid(self):
-        for i in range(self.rows):
-            for j in range(self.cols):
-                # 计算按钮位置
-                ax_button = plt.axes([0.05 + j*0.08, 0.9 - i*0.1, 0.07, 0.07])
-                btn = Button(ax_button, f'{chr(j + 65)}{i + 1}', color='lightblue', hovercolor='skyblue')
-                btn.label.set_fontsize(15)
-                # 绑定点击事件
-                btn.on_clicked(lambda event, row=i, col=j: self.on_click(row, col))
-                self.buttons.append(btn)
-        #plt.suptitle(f'{self.rows}x{self.cols} Button Grid', y=0.95, fontsize=14)
-    
-    # 按钮点击事件处理
-    def on_click(self, row, col):
-        print(f'Button at row {row}, column {col} was clicked')
-        # 改变被点击按钮的颜色
-        idx = row * self.cols + col
-        self.buttons[idx].color = 'lightgreen'
-        self.buttons[idx].hovercolor = 'limegreen'
-        self.fig.canvas.draw()
-
-    # 按钮释放事件处理
-    def on_release(self, row, col):
-        # 改变被点击按钮的颜色
-        idx = row * self.cols + col
-        self.buttons[idx].color = 'lightblue'
-        self.buttons[idx].hovercolor = 'skyblue'
-        self.fig.canvas.draw()
 
 def main():
     #选择Excel文件
@@ -77,6 +42,7 @@ def main():
     cell_dict = {}
     letters = [chr(ord('A') + i) for i in range(8)]  # A-H
 
+    hide_index = []
     y_max = 0
     j = 0
     for letter in letters:
@@ -88,6 +54,9 @@ def main():
                 if cell_value != '':
                     if float(cell_value) > y_max:
                         y_max = float(cell_value)
+                else:
+                    if j not in hide_index:
+                        hide_index.append(j)
                 cell.append(cell_value)
             j += 1
             cell_dict[key] = cell
@@ -104,16 +73,26 @@ def main():
     keys = list(cell_dict.keys())
     x_values = range(1, 31)
     lines = []
+    scatters = []
     # 绘制折线图
     for key in keys:
         y_values = cell_dict[key]
         line, = ax.plot(x_values, y_values, linewidth=2)
         lines.append(line)
+        # 绘制最后一个点的marker
+        scatter = ax.scatter(x_values[-1], y_values[-1], color=line.get_color(), 
+                            marker='o', s=50, zorder=3)
+        scatters.append(scatter)
+
+    for scatter in scatters:
+            scatter.set_visible(False)
 
     # 设置刻度标记大小
     ax.tick_params(axis='both', which='major', labelsize=10)
     ax.set_yticks(range(0, int(y_max) + 2000, 1000))
     ax.set_yticklabels(range(0, int(y_max) + 2000, 1000))
+
+
 
     # 右侧按钮区域
     ax_buttons = fig.add_subplot(gs[0, 1])
@@ -122,24 +101,51 @@ def main():
 
 
     # 按钮点击回调函数
-    button_states = {}
-    for letter in letters:
-        for number in range(1, 13):
-            key = f"{letter}{number}"
-            cell = True
-            button_states[key] = cell
+    button_states = [True] * 96
+    for index in hide_index:
+        button_states[index] = False
 
-    def toggle_line(line_index, index):
+    
+    nc = []
+    nc_num = []
+
+    def toggle_line(index, alp):
         def callback(event):
-            button_states[line_index] = not button_states[line_index]
-            print(line_index)
-            if button_states[line_index]:
-                lines[index].set_alpha(1.0)
-            else:
-                lines[index].set_alpha(0.0)
+            global text_obj
+            if event.button == 1 and index not in nc:
+                button_states[index] = not button_states[index]
+                if button_states[index] and index not in hide_index:
+                    lines[index].set_alpha(1.0)
+                    buttons[index].color = 'lightblue'
+                    buttons[index].hovercolor = '#7fb1b3'
+                elif index not in hide_index:
+                    lines[index].set_alpha(0.0)
+                    buttons[index].color = 'cadetblue'
+                    buttons[index].hovercolor = '#7fb1b3'
+            elif event.button == 3:
+                buttons[index].color = 'yellow' if index not in hide_index else 'dimgray' # 需要优化
+                buttons[index].hovercolor = 'lightyellow' if index not in hide_index else 'dimgray'
+                
+                # 平均值计算
+                if index not in hide_index and index not in nc:
+                    nc.append(index)
+                    nc_num.append(cell_dict[alp][29])
+                    scatters[index].set_visible(True)
+                elif index not in hide_index and index in nc:
+                    nc.remove(index)
+                    nc_num.remove(cell_dict[alp][29])
+                    scatters[index].set_visible(False)
+                    buttons[index].color = 'lightblue'
+                    buttons[index].hovercolor = 'skyblue'
+                # 文本显示
+                if text_obj:
+                    text_obj.remove()
+                text_obj = ax.text(0.5, 0.5, np.average(nc_num) if nc_num else '', fontsize=30)
+
+                
             fig.canvas.draw_idle()
         return callback
-
+    
 
     # 创建8x12按钮网格
     buttons = []
@@ -155,10 +161,13 @@ def main():
                 button_size * 0.415,
                 button_size * 0.9
             ])
-            button = Button(ax_button, f'{chr(row + 65)}{col + 1}', color='lightblue', hovercolor='skyblue')
+            button = Button(ax_button, f'{chr(row + 65)}{col + 1}',
+                            color = 'lightblue' if index not in hide_index else 'dimgray',
+                            hovercolor = '#7fb1b3' if index not in hide_index else 'dimgray')
             button.label.set_fontsize(18)
-            button.on_clicked(toggle_line(f'{chr(row + 65)}{col + 1}', index))
+            button.on_clicked(toggle_line(index, f'{chr(row + 65)}{col + 1}'))
             buttons.append(button)
+            lines[index].set_alpha(0.0) if index in hide_index else lines[index].set_alpha(1.0)
 
 
     #grid = ButtonGrid(8, 12)
